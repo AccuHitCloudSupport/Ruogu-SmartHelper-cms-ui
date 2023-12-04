1fa0f1b8d35701784d547139c1d7b047c902e779
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2023-12-04 17:00:43 +0800

    Update NavbarView.vue

--- src/views/layout/NavbarView.vue
+++ src/views/layout/NavbarView.vue
@@ -8,7 +8,7 @@
     <div v-if="showChat" class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
       <div class="chat-header">
         <!-- WebChat 字樣 -->
-        <img class="me-1 node-icon" src="../../assets/img/chat.png" style="width: 30px; height: 30px; filter: invert(100%);">
+        <img class="me-1 node-icon" src="../../assets/img/chat.png" style="width: 35px; height: 35px; filter: invert(100%);">
         <!-- 展開/收起 按鈕 -->
         <button @click="toggleDialogSize" class="toggle-dialog-button">
           <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
