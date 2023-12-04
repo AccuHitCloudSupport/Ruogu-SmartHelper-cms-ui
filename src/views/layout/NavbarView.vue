7ec761d862de869f81bde9cb3545d02193a69e3d
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2023-12-04 17:52:45 +0800

    Update NavbarView.vue

--- src/views/layout/NavbarView.vue
+++ src/views/layout/NavbarView.vue
@@ -5,20 +5,18 @@
       <li class="nav-item"><router-link to="/permission" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/shield.svg"> 權限管理</router-link></li>
       <li class="nav-item"><router-link to="/dataMangent" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/gridicons.svg"> 資料處理</router-link></li>
     </ul>
-    <div class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
+    <div v-if="showChat" class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
       <div class="chat-header">
         <!-- WebChat 字樣 -->
-        <img class="me-1 node-icon" v-if="!floatingChat" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);" @click="toggleChat">
+        <img class="me-1 node-icon" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
         <!-- 展開/收起 按鈕 -->
-        <button v-if="!floatingChat" @click="toggleDialogSize" class="toggle-dialog-button">
+        <button @click="toggleDialogSize" class="toggle-dialog-button">
           <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
           <img v-else class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
         </button>
       </div>
-      <iframe v-if="!floatingChat" :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
+      <iframe :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
     </div>
-    <!-- 懸浮的 icon -->
-    <img v-if="floatingChat" class="me-1 node-icon floating-chat" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);" @click="toggleDialogSize">
   </div>
 </template>
 
@@ -27,9 +25,8 @@ export default {
   name: 'NavbarView',
   data() {
     return {
-      chatExpanded: true,
-      floatingChat: true,
-      showChat: false
+      showChat: true,
+      chatExpanded: true
     };
   },
   computed: {
@@ -40,9 +37,6 @@ export default {
   methods: {
     toggleDialogSize() {
       this.chatExpanded = !this.chatExpanded;
-    },
-    toggleChat() {
-      this.showChat = !this.showChat;
     }
   }
 }
@@ -120,12 +114,4 @@ export default {
   z-index: 1000;
   color: white;
 }
-
-.floating-chat {
-  position: fixed;
-  bottom: 10px;
-  left: 10px;
-  cursor: pointer;
-}
-  
 </style>
