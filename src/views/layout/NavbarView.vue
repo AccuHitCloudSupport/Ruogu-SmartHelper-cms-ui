6127ceef759c0d3acc24feb5adf727c22fb1eac4
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2023-12-05 11:17:30 +0800

    Update NavbarView.vue

--- src/views/layout/NavbarView.vue
+++ src/views/layout/NavbarView.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
-    <!-- 開關按鈕 -->
-    <button class="toggle-chat-button" @click="toggleChat">
-      <img v-if="!showChat" class="me-1 node-icon" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
-      <img v-else class="me-1 node-icon" src="../../assets/img/close-icon.png" style="width: 25px; height: 25px; filter: invert(100%);">
-    </button>
-    <div v-if="showChat" class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
+  <div class="navbar-vertical navbar-expand">
+    <ul class="py-4 px-3">
+      <!-- 其他項目 -->
+      <li class="nav-item"><router-link to="/permission" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/shield.svg"> 權限管理</router-link></li>
+      <li class="nav-item"><router-link to="/dataMangent" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/gridicons.svg"> 資料處理</router-link></li>
+    </ul>
+    <div class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
       <div class="chat-header">
         <!-- WebChat 字樣 -->
-        <!-- 移除了原先的 chatbot.png -->
+        <img class="me-1 node-icon" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
         <!-- 展開/收起 按鈕 -->
         <button @click="toggleDialogSize" class="toggle-dialog-button">
           <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
           <img v-else class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
         </button>
       </div>
-      <iframe :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
+      <iframe v-show="showChat" :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
     </div>
   </div>
 </template>
@@ -37,43 +37,50 @@ export default {
   methods: {
     toggleDialogSize() {
       this.chatExpanded = !this.chatExpanded;
-    },
-    toggleChat() {
-      this.showChat = !this.showChat;
     }
   }
 }
 </script>
 
-<style>
-/* 開關按鈕樣式 */
-.toggle-chat-button {
-  position: fixed;
-  bottom: 10px;
-  left: 10px;
+<style scoped>
+/* 共用樣式 */
+.nav-item {
+  /* 共用的樣式設定，可以根據需要調整 */
+}
+
+.nav-link {
+  /* 共用的樣式設定，可以根據需要調整 */
+}
+
+.node-icon {
+  margin-right: 1em;
+}
+
+/* 打開聊天按鈕的樣式 */
+.chat-toggle-btn {
   cursor: pointer;
   background-color: transparent;
   border: none;
   display: flex;
   align-items: center;
-  padding: 0;
-  z-index: 1001;
 }
 
-.toggle-chat-button img {
-  margin-right: 1em;
+.chat-toggle-btn-active {
+  background-color: #FFDCB9; /* 變色效果，可根據需求修改 */
+  color: #fff;
+  border: none;
 }
 
 /* 聊天視窗的樣式 */
 .chat-container {
   position: fixed;
   bottom: 0;
-  right: 0;
+  right: 0; /* 控制在最右下角 */
   border: 1px solid #ccc;
   background-color: #fff;
   z-index: 999;
   padding: 0;
-  border-radius: 10px;
+  border-radius: 10px; /* 添加圓角，與表頭相同 */
 }
 
 /* 表頭樣式 */
