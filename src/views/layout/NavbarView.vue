8f2a196b2afaa4a53d244130d6f69522e24bfed8
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2023-12-05 12:29:26 +0800

    Update NavbarView.vue

--- src/views/layout/NavbarView.vue
+++ src/views/layout/NavbarView.vue
@@ -6,14 +6,14 @@
       <li class="nav-item"><router-link to="/dataMangent" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/gridicons.svg"> 資料處理</router-link></li>
     </ul>
     <div class="chat-container">
-      <div class="toggle-dialog-button" @click="toggleDialogSize">
-        <img v-if="!chatExpanded" class="me-1 node-icon" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
-      </div>
+      <button @click="toggleDialogSize" class="toggle-dialog-button">
+        <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
+      </button>
       <div class="chat-header">
         <!-- WebChat 字樣 -->
         <img class="me-1 node-icon" v-if="chatExpanded" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
         <!-- 展開/收起 按鈕 -->
-        <button v-if="chatExpanded" @click="toggleDialogSize" class="me-1 node-icon toggle-dialog-button">
+        <button @click="toggleDialogSize" class="me-1 node-icon toggle-dialog-button">
           <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
           <img v-else class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
         </button>
@@ -43,58 +43,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-/* 共用樣式 */
-.nav-item {
-  /* 共用的樣式設定，可以根據需要調整 */
-}
-
-.nav-link {
-  /* 共用的樣式設定，可以根據需要調整 */
-}
-
-.node-icon {
-  margin-right: 1em;
-}
-
-/* 開關按鈕的樣式 */
-.toggle-dialog-button {
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  padding: 5px;
-  cursor: pointer;
-  background-color: transparent;
-  border: none;
-  z-index: 1000;
-}
-
-/* 聊天視窗的樣式 */
-.chat-container {
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  border: 1px solid #ccc;
-  background-color: #fff;
-  z-index: 999;
-  padding: 0;
-  border-radius: 10px;
-}
-
-/* 表頭樣式 */
-.chat-header {
-  background-color: #0080FF;
-  opacity: 1;
-  height: 60px;
-  margin-bottom: 10px;
-  border-radius: 10px 10px 0 0;
-  border-bottom: 5px solid #0080FF !important;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 10px;
-}
-
-</style>
