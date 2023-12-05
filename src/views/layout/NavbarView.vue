34e02324d98374a3e65ba8cbf2a8ad7f774c2276
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2023-12-05 12:25:34 +0800

    Update NavbarView.vue

--- src/views/layout/NavbarView.vue
+++ src/views/layout/NavbarView.vue
@@ -2,33 +2,23 @@
   <div class="navbar-vertical navbar-expand">
     <ul class="py-4 px-3">
       <!-- 其他項目 -->
-      <li class="nav-item">
-        <router-link to="/permission" class="nav-link" href="#">
-          <img class="me-1 node-icon" src="../../assets/img/shield.svg"> 權限管理
-        </router-link>
-      </li>
-      <li class="nav-item">
-        <router-link to="/dataMangent" class="nav-link" href="#">
-          <img class="me-1 node-icon" src="../../assets/img/gridicons.svg"> 資料處理
-        </router-link>
-      </li>
+      <li class="nav-item"><router-link to="/permission" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/shield.svg"> 權限管理</router-link></li>
+      <li class="nav-item"><router-link to="/dataMangent" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/gridicons.svg"> 資料處理</router-link></li>
     </ul>
-    <!-- 開關按鈕 -->
-    <button @click="toggleDialogSize" class="toggle-dialog-button">
-      <img class="me-1 node-icon" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
-    </button>
-    <div v-if="showChat" class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
+    <div class="chat-container">
+      <div class="toggle-dialog-button" @click="toggleDialogSize">
+        <img v-if="!chatExpanded" class="me-1 node-icon" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
+      </div>
       <div class="chat-header">
         <!-- WebChat 字樣 -->
-        <div>
-          <!-- 原本的展開/收起 按鈕 -->
-          <button @click="toggleDialogSize" class="toggle-dialog-button">
-            <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
-            <img v-else class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
-          </button>
-        </div>
+        <img class="me-1 node-icon" v-if="chatExpanded" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
+        <!-- 展開/收起 按鈕 -->
+        <button v-if="chatExpanded" @click="toggleDialogSize" class="me-1 node-icon toggle-dialog-button">
+          <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
+          <img v-else class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
+        </button>
       </div>
-      <iframe :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
+      <iframe v-if="chatExpanded" :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
     </div>
   </div>
 </template>
@@ -38,7 +28,6 @@ export default {
   name: 'NavbarView',
   data() {
     return {
-      showChat: true,
       chatExpanded: true
     };
   },
