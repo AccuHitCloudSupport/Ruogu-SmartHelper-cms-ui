7b655e242f581c067c172b73cd3bbc6ddf4c8842
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2023-12-06 16:57:07 +0800

    Update NavbarView.vue

--- src/views/layout/NavbarView.vue
+++ src/views/layout/NavbarView.vue
@@ -6,6 +6,13 @@
       <li class="nav-item"><router-link to="/dataMangent" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/gridicons.svg"> 資料處理</router-link></li>
       <!-- 打開聊天按鈕 -->
       <li class="nav-item">
+        <button @click="toggleDialogSize" class="toggle-dialog-button">
+          <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
+          <img v-else class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
+        </button>
+      </li>
+      <!-- 打開聊天按鈕 -->
+      <li class="nav-item">
         <button @click="toggleChat" :class="{ 'chat-toggle-btn-active': showChat }" class="nav-link">
           <img class="me-1 node-icon" src="../../assets/img/cha-bubbles-two-svgrepo-com.svg" style="width: 15px; height: 15px;">
           對話測試
@@ -15,41 +22,13 @@
     <div v-if="showChat" class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
       <div class="chat-header">
         <!-- 移除關閉按鈕 -->
-        <!-- 展開/收起 按鈕 -->
-        <button @click="toggleDialogSize" class="toggle-dialog-button">
-          <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
-          <img v-else class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
-        </button>
       </div>
       <iframe :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'NavbarView',
-  data() {
-    return {
-      showChat: false,
-      chatExpanded: true
-    };
-  },
-  computed: {
-    chatSrc() {
-      return `https://webchat.botframework.com/embed/testing-smarthelper-chatbot-bot?s=${process.env.VUE_APP_BOT_TOKEN}`;
-    }
-  },
-  methods: {
-    toggleChat() {
-      this.showChat = !this.showChat;
-    },
-    toggleDialogSize() {
-      this.chatExpanded = !this.chatExpanded;
-    }
-  }
-}
-</script>
+<!-- 其他部分保持不變 -->
 
 <style scoped>
 /* ... */
