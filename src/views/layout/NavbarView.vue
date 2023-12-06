a429f16f2906fbc1d46f47e52cbe4ed3ed597fab
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2023-12-06 15:38:21 +0800

    Update NavbarView.vue

--- src/views/layout/NavbarView.vue
+++ src/views/layout/NavbarView.vue
@@ -4,129 +4,29 @@
       <!-- 其他項目 -->
       <li class="nav-item"><router-link to="/permission" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/shield.svg"> 權限管理</router-link></li>
       <li class="nav-item"><router-link to="/dataMangent" class="nav-link" href="#"><img class="me-1 node-icon" src="../../assets/img/gridicons.svg"> 資料處理</router-link></li>
+      <!-- 打開聊天按鈕 -->
+      <li class="nav-item">
+        <button @click="toggleChat" :class="{ 'chat-toggle-btn-active': showChat }" class="nav-link">
+          <img class="me-1 node-icon" src="../../assets/img/cha-bubbles-two-svgrepo-com.svg" style="width: 15px; height: 15px;">
+          對話測試
+        </button>
+      </li>
     </ul>
-    <div class="chat-container" :class="{ 'expanded': chatExpanded }">
+    <div v-if="showChat" class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
       <div class="chat-header">
-        <!-- WebChat 字樣 -->
-        <img class="me-1 node-icon" src="../../assets/img/chatbot.png" style="width: 35px; height: 35px; filter: invert(100%);">
-        <!-- 展開/收起 按鈕容器 -->
-        <div class="toggle-dialog-container">
-          <!-- 展開 按鈕 -->
-          <button v-if="!chatExpanded" @click="toggleDialogSize" class="toggle-dialog-button">
-            <img class="icon" src="../../assets/img/chat.png">
-          </button>
-          <!-- 收回 按鈕 -->
-          <button v-else @click="hideDialog" class="toggle-dialog-button">
-            <img class="icon" src="../../assets/img/down.png">
-          </button>
-        </div>
-        <!-- 展開按鈕 -->
-        <button v-if="!chatExpanded" @click="showDialog" class="toggle-dialog-button">
-          <img class="icon" src="../../assets/img/up.png">
+        <!-- 新增下縮按鈕 -->
+        <button @click="toggleDialogHeight" class="toggle-dialog-height-button">
+          <img v-if="!chatExpanded" class="me-1 node-icon" src="../../assets/img/expand.png" style="width: 15px; height: 15px; filter: invert(100%);">
+        </button>
+        <!-- 展開/收起 按鈕 -->
+        <button @click="toggleDialogSize" class="toggle-dialog-button">
+          <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
+          <img v-else class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
         </button>
       </div>
-      <iframe v-if="chatExpanded" :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
+      <iframe :src="chatSrc" style='min-width: 400px; width: 100%; min-height: 500px;' class="orange-background"></iframe>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'NavbarView',
-  data() {
-    return {
-      chatExpanded: true
-    };
-  },
-  computed: {
-    chatSrc() {
-      return `https://webchat.botframework.com/embed/testing-smarthelper-chatbot-bot?s=${process.env.VUE_APP_BOT_TOKEN}`;
-    }
-  },
-  methods: {
-    toggleDialogSize() {
-      this.chatExpanded = !this.chatExpanded;
-    },
-    hideDialog() {
-      this.chatExpanded = false;
-    },
-    showDialog() {
-      this.chatExpanded = true;
-    }
-  }
-}
-</script>
-
-<style scoped>
-/* 共用樣式 */
-.nav-item {
-  /* 共用的樣式設定，可以根據需要調整 */
-}
-
-.nav-link {
-  /* 共用的樣式設定，可以根據需要調整 */
-}
-
-.node-icon {
-  margin-right: 1em;
-}
-
-<!-- 聊天視窗的樣式 -->
-.chat-container {
-  position: fixed;
-  bottom: 0;
-  right: 0; /* 控制在最右下角 */
-  border: 1px solid #ccc;
-  background-color: #fff;
-  z-index: 999;
-  padding: 0;
-  border-radius: 10px; /* 添加圓角，與表頭相同 */
-  overflow: hidden; /* 隱藏多餘內容 */
-  height: 0; /* 修改這裡 */
-  transition: all 0.3s; /* 添加過渡效果 */
-}
-
-/* 表頭樣式 */
-.chat-header {
-  background-color: #0080FF;
-  opacity: 1;
-  height: 60px;
-  margin-bottom: 10px;
-  border-radius: 10px 10px 0 0;
-  border-bottom: 5px solid #0080FF !important;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 10px;
-}
-
-/* 開關按鈕 */
-.toggle-dialog-container {
-  position: absolute;
-  top: 0;
-  right: 0px;
-  display: flex;
-  align-items: center;
-}
-
-.toggle-dialog-button {
-  padding: 5px 10px;
-  cursor: pointer;
-  background-color: transparent;
-  border: none;
-  z-index: 1000;
-  color: white;
-}
-
-.icon {
-  width: 35px;
-  height: 35px;
-  filter: invert(100%);
-}
-
-/* 展開時的樣式 */
-.expanded {
-  height: 560px; /* 調整展開時的高度 */
-}
-</style>
+<!-- ... (剩餘部分保持不變) -->
