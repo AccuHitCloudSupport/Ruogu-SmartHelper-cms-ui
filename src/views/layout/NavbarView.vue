817cf3fb8ab80df80a59c1538dd8a90ac940aeaf
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2023-12-07 12:51:09 +0800

    Update NavbarView.vue

--- src/views/layout/NavbarView.vue
+++ src/views/layout/NavbarView.vue
@@ -13,9 +13,7 @@
       </li>
     </ul>
     <div v-if="showChat" class="chat-container" :style="{ height: chatExpanded ? 'auto' : '45px' }">
-      <div class="chat-header">
-        <!-- 移除close按鈕和圖片 -->
-        <!-- 只保留展開/收起 按鈕 -->
+      <div v-if="chatExpanded" class="chat-header">
         <button @click="toggleDialogSize" class="toggle-dialog-button">
           <img v-if="chatExpanded" class="me-1 node-icon" src="../../assets/img/down.png" style="width: 15px; height: 15px; filter: invert(100%);">
           <img v-else class="me-1 node-icon" src="../../assets/img/up.png" style="width: 15px; height: 15px; filter: invert(100%);">
@@ -113,7 +111,7 @@ export default {
 .toggle-dialog-button {
   position: absolute;
   top: 0;
-  left: 0; /* 調整 left 屬性 */
+  right: 0px;
   padding: 5px 10px;
   cursor: pointer;
   background-color: transparent;
