0c53096fd7e589e47101c2e5c05a66f2d3fe681e
AccuHitCloudSupport <139300336+AccuHitCloudSupport@users.noreply.github.com>
2024-04-15 11:36:27 +0800

    Update HeaderView.vue

--- src/views/layout/HeaderView.vue
+++ src/views/layout/HeaderView.vue
@@ -3,7 +3,7 @@
         <div class="navbar-collapse">
             <div class="d-flex align-items-center justify-content-between">
                 <div class="navbar-logo">
-                    <img src="../../assets/img/logo2.png" width="113" alt="logo">
+                    <img src="../../assets/img/LOGO.png" width="113" alt="logo">
                 </div>
 
                 <ul class="d-flex align-items-center">
